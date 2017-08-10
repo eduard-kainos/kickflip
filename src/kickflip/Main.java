abb828613d65101d291429917f47cabf56eb984c
Shannon Dorrian <shannond@kainos.com>
2017-08-10 14:59:41 +0100

    user story 3 started

--- src/kickflip/Main.java
+++ src/kickflip/Main.java
@@ -33,6 +33,8 @@ public class Main {
 				repeatInt ++;
 				System.exit(0);
 				break;
+			case 4:
+				insertSalesEmployee();
 			default:
 				break;	
 			}
@@ -44,7 +46,8 @@ public class Main {
 		System.out.println("Please select an option:"
 			+ " \n1: Add an employee."
 			+ " \n2: Show employee table."
-			+ " \n3: Exit");
+			+ " \n3: Exit." 
+			+ " \n4: Add sales employee.");
 		}
 
 	public static void select() {
@@ -101,10 +104,10 @@ public class Main {
 		System.out.println("Please enter the employees national insurance number (9 characters)");
 		String nin = sc.nextLine();
 
-		System.out.println("Please enter the employees bank account number");
+		System.out.println("Please enter the employees bank account number (8 digits)");
 		String bankAccount = sc.nextLine();
 
-		System.out.println("Please enter the employees sort code number");
+		System.out.println("Please enter the employees sort code number (6 digits)");
 		String sortCode = sc.nextLine();
 
 		System.out.println("Please enter the employees department ID \n1 for Evolve \n2 for Enterprise \n3 for Government");
@@ -127,6 +130,97 @@ public class Main {
 
 			// execute the preparedstatement
 			preparedStmt.execute();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			Database.closeConnection(conn);
+			System.out.println( name + " has been successfully added.");
+		}
+		
+	}
+	
+
+	public static void insertSalesEmployee() {
+		conn = null;
+		statement = null;
+		preparedStmt = null;
+
+		Scanner sc = new Scanner(System.in);
+
+		String query = "INSERT INTO "
+				+ "employee(name, address, initialSalary, nin, bankAccountNo, sortCode, departmentID)"
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+		System.out.println("Please enter the sales employees name");
+		String name = sc.nextLine();
+
+		System.out.println("Please enter the sales employees address");
+		String address = sc.nextLine();
+
+		System.out.println("Please enter the sales employee starting salary");
+		BigDecimal initialSalary = sc.nextBigDecimal();
+		sc.nextLine();
+
+		System.out.println("Please enter the sales employees national insurance number (9 characters)");
+		String nin = sc.nextLine();
+
+		System.out.println("Please enter the sales employees bank account number (8 digits)");
+		String bankAccount = sc.nextLine();
+
+		System.out.println("Please enter the sales employees sort code number (6 digits)");
+		String sortCode = sc.nextLine();
+
+		System.out.println("Please enter the sales employees department ID \n1 for Evolve \n2 for Enterprise \n3 for Government");
+		int departID = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("Please enter the sales employee commission rate");
+		BigDecimal  commission = sc.nextBigDecimal();
+		sc.nextLine();
+		
+		System.out.println("Please enter the sales employee sales total");
+		BigDecimal  salesTotal = sc.nextBigDecimal();
+		sc.nextLine();
+		
+		String query2 = "INSERT INTO "
+				+ "salesEmployee(employeeNumber, commissionRate, salesTotal)"
+				+ "VALUES (?, ?, ?);";
+		
+
+		try {
+			conn = Database.getConnection();
+			statement = conn.prepareStatement(query);
+
+			// create the mysql insert preparedstatement
+			preparedStmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			preparedStmt.setString(1, name);
+			preparedStmt.setString(2, address);
+			preparedStmt.setBigDecimal(3, initialSalary);
+			preparedStmt.setString(4, nin);
+			preparedStmt.setString(5, bankAccount);
+			preparedStmt.setString(6, sortCode);
+			preparedStmt.setInt(7, departID);
+
+			// execute the preparedstatement
+			preparedStmt.execute();
+
+			ResultSet resultSet = preparedStmt.getGeneratedKeys();
+			int empNumCurrent = 0;
+		
+			while(resultSet.next()) {
+				empNumCurrent = resultSet.getInt(1);
+			}
+			
+			statement = conn.prepareStatement(query2);
+			preparedStmt = conn.prepareStatement(query2);
+			preparedStmt.setInt(1, empNumCurrent);
+			preparedStmt.setBigDecimal(2, commission);
+			preparedStmt.setBigDecimal(3, salesTotal);
+			 
+			preparedStmt.execute();
+			
 		} catch (SQLException e1) {
 			e1.printStackTrace();
 		} catch (Exception e) {
