e0563bbbf87aafc16fb6e9f83b7feee6cb9fe411
Eduard <eduardg@kainos.com>
2017-08-10 10:49:35 +0100

    Adds mysql connection.

--- src/kickflip/Main.java
+++ src/kickflip/Main.java
@@ -1,9 +1,24 @@
 package kickflip;
 
+import java.sql.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/kickflip", "root", "p@ssword");
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("SELECT employeeNumber, name FROM employee LIMIT 5");
+			while (rs.next()) {
+				String out = String.format("%s has the following employee number: %s.", rs.getString("name"),
+						rs.getString("employeeNumber"));
+				System.out.println(out);
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 }
